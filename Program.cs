03a9e2d412111506b94a8187459e6ea1f779e28c
Antonio <dyachkov1984@inbox.ru>
2022-07-16 16:53:02 +0300

    Case 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*С клавиатуры вводится целое число. Вывести квадрат числа
+int a = 2;
+int c = 4;
+int g = a + c;
+Console.WriteLine("{0}+{1}={2}", a, c, g);
+double
+*/
+
+int a;
+int b;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+b=a*a;
+Console.WriteLine($"{a}^2={b}");
